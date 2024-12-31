7eb0fa8e543f1e6d2f09acfbfb2075ca76038ec5
Jiayi Wang <jjiayu889@gmail.com>
2024-12-31 11:47:52 +0800

    clean header file

--- src/Kuavo4pro.h
+++ src/Kuavo4pro.h
@@ -14,30 +14,3 @@ public:
 };
 
 } // namespace mc_robots
-
-//Previous way, now abandoned
-// extern "C"
-// {
-//   ROBOT_MODULE_API void MC_RTC_ROBOT_MODULE(std::vector<std::string> & names)
-//   {
-//     // Names of the robots exported by this module
-//     names = {"Kuavo4pro"};
-//   }
-//   ROBOT_MODULE_API void destroy(mc_rbdyn::RobotModule * ptr)
-//   {
-//     delete ptr;
-//   }
-//   ROBOT_MODULE_API mc_rbdyn::RobotModule * create(const std::string & name)
-//   {
-//     // At this point name must be one of the supported robots
-//     if(name == "Kuavo4pro")
-//     { 
-//       return new mc_robots::Kuavo4pro(); 
-//     }
-//     else
-//     {
-//       mc_rtc::log::error("Kuavo4pro module Cannot create an object of type {}", name);
-//       return nullptr;
-//     }
-//   }
-// }
